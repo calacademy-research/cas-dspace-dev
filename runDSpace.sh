bb62046f47e4ec6d7fb3a783dcb22e6ad6cc996e
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2019-06-17 13:29:19 -0700

    Fixed dir in Launch Script

--- runDSpace.sh
+++ runDSpace.sh
@@ -2,7 +2,7 @@
 
 start() {
   cd
-  cd Documents/GitHub/cas-dspace-dev/dspace-docker/DSpace-Docker-Images/docker-compose-files/dspace-compose
+  cd Documents/GitHub/cas-dspace-dev/dspace-docker-harrison-initial/DSpace-Docker-Images/docker-compose-files/dspace-compose
   docker-compose -p d6 -f docker-compose.yml -f d6.override.yml up -d
   command
   cd
@@ -14,7 +14,7 @@ command() {
 
 stop() {
   cd
-  cd Documents/GitHub/cas-dspace-dev/dspace-docker/DSpace-Docker-Images/docker-compose-files/dspace-compose
+  cd Documents/GitHub/cas-dspace-dev/dspace-docker-harrison-initial/DSpace-Docker-Images/docker-compose-files/dspace-compose
   docker-compose -p d6 -f docker-compose.yml -f d6.override.yml down
   cd
 }
