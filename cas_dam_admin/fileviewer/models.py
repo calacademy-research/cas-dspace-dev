8a0fa22f0912cc05910985786b6a5931335dbfc0
Dash <dash.peters@gmail.com>
2019-06-19 16:58:38 -0700

    Remove commented demo code

--- cas_dam_admin/fileviewer/models.py
+++ cas_dam_admin/fileviewer/models.py
@@ -6,7 +6,6 @@ from django.core.validators import FileExtensionValidator
 
 
 class CSVDocument(models.Model):
-    # description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/', validators=[FileExtensionValidator(allowed_extensions=['csv'])])
     path = models.CharField(max_length=255)  # max_length is required, we could make it longer if needed
     uploaded_at = models.DateTimeField(auto_now_add=True)
