80089b041839068737769dc4b28fdd688aed47a6
Dash <dash.peters@gmail.com>
2019-06-19 14:12:14 -0700

    Add docstring to save_file_to_model

--- cas_dam_admin/fileviewer/models.py
+++ cas_dam_admin/fileviewer/models.py
@@ -14,6 +14,10 @@ class CSVDocument(models.Model):
     csv_file = None
 
     def save_file_to_model(self):
+        """
+        Opens the path to the csv document and reads it with csv.DictReader, then saves it to the document object
+        :return:
+        """
         with open(self.document.path, "r") as f:
             self.csv_file = csv.DictReader(f)
             logging.info("saving csv file", self.csv_file)
