186d76b7ef85f18b5ebd99adb17c164209b12440
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2019-06-22 15:58:26 -0700

    Cleaned up and added some new functions

--- gcloud.py
+++ gcloud.py
@@ -5,56 +5,113 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-# SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+class Gcloud:
+    '''
+    The gcloud functions are wrapped in a class to make it easier to interface with
 
-    service = build('drive', 'v3', credentials=creds)
+    Files used for Authentication, stored in the authdir attribute
 
-    """
-    Format of the folder_id is weird. Ex. 'root' gives error but "'root'" works
-    Also this function searches the entire tree ** big bad
-    """
-    def search_for_children(folder_id, depth=1):
+        tokenFile: pickle file used to remember login, if deleted, will prompt for login next time and rebuild file
 
-        query = "%s in parents" % folder_id
+        credentialsFile: json file that contains credentials that allow access to Google Drive API
+    '''
+    tokenFile = 'token.pickle'
+    credentialsFile = 'credentials.json'
+
+    def __init__(self, authdir=''):
+        # Authdir is the location of the authentication files
+
+        self.authdir = authdir
+        self.service = self.authenticate()
+
+    def authenticate(self):
+
+        '''
+        Authenticates google service and returns the service that can be used for all subsequent api calls
+            Will build token.pickle file if not already built, remove this feature after testing.
+        '''
+
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(self.authdir + Gcloud.tokenFile):
+            with open(self.authdir + Gcloud.tokenFile, 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.authdir + Gcloud.credentialsFile, SCOPES)
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open(self.authdir + Gcloud.tokenFile, 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('drive', 'v3', credentials=creds)
+
+        return service
+
+    def perform_search(self, query, file_count):
         page_token = None
+
+        print("Running search: "+query)
+
         while True:
-            response = service.files().list(q= query,
-                                                  spaces='drive',
-                                                  fields='nextPageToken, files(id, name)',
-                                                  pageToken=page_token).execute()
+            response = self.service.files().list(q=query,
+                                                 spaces='drive',
+                                                 fields='nextPageToken, files(id, name)',
+                                                 pageToken=page_token).execute()
+            files = []
             for file in response.get('files', []):
                 # Process change
-                print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
-                pass
+                files.append([file.get('name'), file.get('id')])
+
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
-    search_for_children("'root'")
+        print("Successfully ran search: "+query)
+        return files[:file_count]
+
+
+    def children_search(self, folder_id, file_count=10):
+        '''
+        takes in the id of a folder in google drive and spits out (file_count) files
+
+        folder_id is google drive id, referred to as driveId in API, which is a piece of metadata of each file
+        '''
+        query = "'%s' in parents" % folder_id
+        return self.perform_search(query, file_count)
+
+
+    def ID_from_name(self, file_name, file_count=10):
+        '''
+        takes in a file name and returns all files from google drive with that name
+        '''
+
+        query = "name = '%s'" % file_name
+        results =  self.perform_search(query, file_count)
+
+        if len(results) > 1:
+            return results
+
+        elif results == []:
+            return []
+
+        return results[0][1]
 
 if __name__ == '__main__':
-    main()
+    import pprint
+    '''
+    Assumes token.pickle and credentials.json is in directory
+    '''
+
+    g = Gcloud()
+    files = g.children_search('root', 5)
+    print("[File Name, File ID ]")
+    print("")
+    pprint.pprint(files)
