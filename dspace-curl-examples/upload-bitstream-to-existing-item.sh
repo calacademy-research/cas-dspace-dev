b74de3cba17c5a73627564085690f83ca62e55fa
Dash <dash.peters@gmail.com>
2019-06-24 11:56:08 -0700

    Fix curl example for adding bitstream

--- dspace-curl-examples/upload-bitstream-to-existing-item.sh
+++ dspace-curl-examples/upload-bitstream-to-existing-item.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 curl -k -4 \
   --cookie "JSESSIONID=92CF8DB302C9F4E95AE9B17458AC3AB1" \
   -H "accept: application/json" \
-	-X POST --url "localhost:8080/rest/items/b451f94e-2ac6-4807-a121-868e42828ef0/bitstreams?name=test.pdf" \
+  -X POST --url "localhost:8080/rest/items/b451f94e-2ac6-4807-a121-868e42828ef0/bitstreams?name=file.pdf" \
   -T "./file.pdf"
 
 # -k insecure, -4 ipv4, cookie same as always
