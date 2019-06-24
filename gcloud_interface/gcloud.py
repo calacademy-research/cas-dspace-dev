4889c20cf3f9bddde2c8a2fe26dee9b180b629a4
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2019-06-24 15:04:01 -0700

    Now prints a tree of a given directory

--- gcloud_interface/gcloud.py
+++ gcloud_interface/gcloud.py
@@ -5,6 +5,8 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+import pprint
+
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 class Gcloud:
@@ -64,28 +66,30 @@ class Gcloud:
         while True:
             response = self.service.files().list(q=query,
                                                  spaces='drive',
-                                                 fields='nextPageToken, files(id, name)',
+                                                 #fields='nextPageToken, files(id, name)',
                                                  pageToken=page_token).execute()
             files = []
             for file in response.get('files', []):
                 # Process change
-                files.append([file.get('name'), file.get('id')])
-
+                #files.append([file.get('name'), file.get('id')])
+                files.append(file)
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
         print("Successfully ran search: "+query)
-        return files[:file_count]
+        return files
 
 
-    def children_search(self, folder_id, file_count=10):
+    def children_search(self, folder_id, file_count=10000):
         '''
         takes in the id of a folder in google drive and spits out (file_count) files
 
         folder_id is google drive id, referred to as driveId in API, which is a piece of metadata of each file
         '''
         query = "'%s' in parents" % folder_id
-        return self.perform_search(query, file_count)
+        results =  self.perform_search(query, file_count)
+        return results
+        #return [[file.get('name'), file.get('id')] for file in results]
 
 
     def ID_from_name(self, file_name, file_count=10):
@@ -97,6 +101,7 @@ class Gcloud:
 
         query = "name = '%s'" % file_name
         results =  self.perform_search(query, file_count)
+        #id_name_tuples  = [(file.get('name'), file.get('id')) for file in results]
 
         if len(results) > 1:
             return results
@@ -104,20 +109,53 @@ class Gcloud:
         elif results == []:
             return []
 
-        return results[0][1]
+        return results[0]['id']
+
+    def create_directory_tree(self, fileID, depth):
+        if depth == 0:
+            return []
+        else:
+            children = self.children_search(fileID)
+            next_layer = []
+            for child in children:
+                if self.is_folder(child):
+                    next_layer.append([child['name'], self.create_directory_tree(child['id'], depth - 1)])
+                else:
+                    next_layer.append([child['name']])
+            return next_layer
+
+            #return [[child['name'], create_directory_tree(self.children_search(child['id']), depth - 1)] if self.is_folder(child) else [child['name']] for child in children]
+
+
+
+    def is_folder(self, file_data):
+        if file_data['mimeType'] == 'application/vnd.google-apps.folder':
+            return True
+
+        else:
+            return False
 
 if __name__ == '__main__':
-    import pprint
+
+    def print_tree(tree, indent=''):
+        for branch in tree:
+            if type(branch) == list and branch != []:
+                print_tree(branch, indent+ '     ')
+            else:
+                if branch != []:
+                    print(indent+ str(branch))
+
     '''
     Assumes token.pickle and credentials.json are in directory
     '''
 
 
     '''
-    Sample Test prints 5 files from root directory of google drive
+    Sample Test prints out a tree of everything within that folder of the google drive
     '''
     g = Gcloud()
-    files = g.children_search('root', 5)
-    print("[File Name, File ID ]")
-    print("")
-    pprint.pprint(files)
+    file_name = 'bigFilewithAll'
+    folder_id = g.ID_from_name(file_name)
+    files = g.create_directory_tree(folder_id, 3)
+    print(files)
+    print_tree(files)
