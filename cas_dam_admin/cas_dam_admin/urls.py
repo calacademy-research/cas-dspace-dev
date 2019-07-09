fb78ff5b389ee14bc69fed64553ec720d4c47751
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2019-07-09 09:58:22 -0700

    Added path to api app

--- cas_dam_admin/cas_dam_admin/urls.py
+++ cas_dam_admin/cas_dam_admin/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('fileviewer/', include('fileviewer.urls')),
     path('localuploader/', include('localuploader.urls')),
     path('admin/', admin.site.urls),
