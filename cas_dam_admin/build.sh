936bb115d5e2cbdc850c1797256cc14c005a65a4
Dash Peters <dpeters@calacademy.org>
2021-04-06 10:35:05 -0700

    remove sudo from build and run commands

--- cas_dam_admin/build.sh
+++ cas_dam_admin/build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo docker build ./ -t joe_cas_dam_admin
+docker build ./ -t joe_cas_dam_admin
+
--- cas_dam_admin/run.sh
+++ cas_dam_admin/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo docker run -d --restart unless-stopped -p 8000:8000 --env DSPACE_URL=http://ibss-assets/rest joe_cas_dam_admin
+docker run -d --restart unless-stopped -p 8000:8000 --env DSPACE_URL=http://ibss-assets/rest -v /home:/home --name dspace_uploader joe_cas_dam_admin
+
