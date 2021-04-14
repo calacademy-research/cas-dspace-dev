25b773374123a6db3e5fae02da6a8618375c37b4
root <root@ibss-assets2.calacademy.org>
2021-04-14 16:43:42 -0700

    Fix run.sh

--- cas_dam_admin/run.sh
+++ cas_dam_admin/run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-docker run -d --restart unless-stopped -p 8000:8000 --env DSPACE_URL=http://ibss-assets/rest -v /home:/home --name dspace_uploader joe_cas_dam_admin
+docker run -d \
+	--restart unless-stopped \
+	-p 8000:8000 \
+	-h `hostname` \
+	--env DSPACE_URL=http://ibss-assets/rest \
+	-v /home:/home \
+	-v db.sqlite3:/cas_dam_admin/database/db.sqlite \
+	--name dspace_uploader \
+	joe_cas_dam_admin
 
